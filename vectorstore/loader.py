3cb82b21f0925a48934ae0652c318c68fcbae0da
Prashant Singh <prshnnt@gmail.com>
2026-07-27 23:13:38 +0530

    feat: add vectorstore loader stubs and chunk-length config

---
+++ vectorstore/loader.py
@@ -0,0 +1,12 @@
+def load_csv(path):
+    pass
+def load_json(path):
+    pass
+def get_json_schema(obj):
+    pass
+def load_pdf(path):
+    pass
+def load_docx(path):
+    pass
+def load_txt(path):
+    pass
