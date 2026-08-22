8c1ed87728baf173585c6a6589dc643a04121ae2
Prashant Singh <prshnnt@gmail.com>
2026-08-22 23:30:58 +0530

    changed

--- vectorstore/loaders.py
+++ vectorstore/loaders.py
@@ -5,16 +5,6 @@ from langchain_core.documents import Document
 
 class Convert:
     @staticmethod
-    def pdf_to_markdown(path:str|List[str],output_dir="output/"):
-        """
-        Convert PDF(s) into the requested output format(s).
-        
-            Args:
-                input_path: One or more input PDF file paths or directories
-                output_dir: Directory where output files are written. Default: input file directory
-        """
-        odlp.convert(input_path=path,output_dir=output_dir,format="markdown")
-    @staticmethod
     def langchain_load_pdf(path:str|List[str],format="text"):
         loader = OpenDataLoaderPDFLoader(
             file_path=path,
